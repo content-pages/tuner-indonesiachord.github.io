5de234c5522ccb06ca4bf9d65cfd136d7163365a
content-pages <161290566+content-pages@users.noreply.github.com>
2024-04-25 14:18:21 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const Application=function(){this.initA4()
 this.tuner=new Tuner(this.a4)
 this.notes=new Notes('.notes',this.tuner)
 this.meter=new Meter('.meter')
+this.frequencyBars=new FrequencyBars('.frequency-bars')
 this.update({name:'A',frequency:this.a4,octave:4,value:69,cents:0})}
 Application.prototype.initA4=function(){this.$a4=document.querySelector('.a4 span')
 this.a4=parseInt(localStorage.getItem('a4'))||440
@@ -17,6 +18,10 @@ self.tuner.middleA=a4
 self.notes.createNotes()
 self.update({name:'A',frequency:self.a4,octave:4,value:69,cents:0})
 localStorage.setItem('a4',a4)})})
+this.updateFrequencyBars()}
+Application.prototype.updateFrequencyBars=function(){if(this.tuner.analyser){this.tuner.analyser.getByteFrequencyData(this.frequencyData)
+this.frequencyBars.update(this.frequencyData)}
+requestAnimationFrame(this.updateFrequencyBars.bind(this))}
 Application.prototype.update=function(note){this.notes.update(note)
 this.meter.update((note.cents/50)*45)}
 Application.prototype.toggleAutoMode=function(){this.notes.toggleAutoMode()}
